069b100576786dcbcb99fafc67a7255da2468bae
Ajinkya Taranekar <ajinkyataranekar@gmail.com>
2021-06-12 00:08:15 +0530

    Address: Added fields (country, latitude, longitude)

--- src/main/java/com/hack/azure/mediknot/entity/Address.java
+++ src/main/java/com/hack/azure/mediknot/entity/Address.java
@@ -13,12 +13,18 @@ public class Address {
     private String street;
     private String city;
     private String state;
+    private String country;
     private Integer pinCode;
+    private Float latitude;
+    private Float longitude;
 
-    public Address(String street, String city, String state, Integer pinCode) {
+    public Address(String street, String city, String state, String country, Integer pinCode, Float latitude, Float longitude ) {
         this.street = street;
         this.city = city;
         this.state = state;
+        this.country = country;
         this.pinCode = pinCode;
+        this.latitude = latitude;
+        this.longitude = longitude;
     }
 }
