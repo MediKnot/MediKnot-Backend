232eee9ca7c9779f644eaef5dad65e07a41ce7a9
Ajinkya Taranekar <ajinkyataranekar@gmail.com>
2021-06-12 00:37:08 +0530

    Doctor: Added fields

--- src/main/java/com/hack/azure/mediknot/entity/Doctor.java
+++ src/main/java/com/hack/azure/mediknot/entity/Doctor.java
@@ -1,9 +1,18 @@
 package com.hack.azure.mediknot.entity;
 
+import java.util.List;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class Doctor extends User{
+    private List<String> specialization;	
+    private List<String> degree;
+    private List<String> medicalCouncilAffilation;
+    private List<String> clinicName;
+    private String registrationNumber;
+    private Integer rating;
+    private Integer yearOfExperience;
 }
