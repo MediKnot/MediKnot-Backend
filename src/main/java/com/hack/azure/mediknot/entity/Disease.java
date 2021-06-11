9d894454362a882d8e52ae9bb463db14e44f7ee0
Malay Saxena <dynamic.malay@gmail.com>
2021-06-11 23:34:16 +0530

    [ADD] Disease

--- src/main/java/com/hack/azure/mediknot/entity/Disease.java
+++ src/main/java/com/hack/azure/mediknot/entity/Disease.java
@@ -1,4 +1,13 @@
 package com.hack.azure.mediknot.entity;
 
+import java.util.List;
+
 public class Disease {
+    private Integer id;
+    private String name;
+    private String introduction;
+    private List<String> symptoms;
+    private List<String> causes;
+    private List<String> diagnosis;
+    private List<String> cures;
 }
--- src/main/java/com/hack/azure/mediknot/entity/Patient.java
+++ src/main/java/com/hack/azure/mediknot/entity/Patient.java
@@ -1,5 +1,8 @@
 package com.hack.azure.mediknot.entity;
 
+import java.util.List;
+
 public class Patient extends User{
+    List<MedicalEvent> timeline;
 
 }
