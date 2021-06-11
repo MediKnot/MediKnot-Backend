54738a9f85f82b6006182519f0b1ed4519ef2996
Malay Saxena <malay.saxena@intellicus.com>
2021-06-11 14:39:31 +0530

    first commit

---
+++ src/main/java/com/hack/azure/mediknot/MediKnotApplication.java
@@ -0,0 +1,13 @@
+package com.hack.azure.mediknot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediKnotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediKnotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hack/azure/mediknot/MediKnotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hack.azure.mediknot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediKnotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
