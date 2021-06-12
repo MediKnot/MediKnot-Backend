c82a1fa0378e9e01578adf7ca6cb82ab3d61351b
Malay Saxena <dynamic.malay@gmail.com>
2021-06-12 18:01:11 +0530

    MedicalDto updated

--- src/main/java/com/hack/azure/mediknot/dto/MedicalDto.java
+++ src/main/java/com/hack/azure/mediknot/dto/MedicalDto.java
@@ -1,21 +1,29 @@
 package com.hack.azure.mediknot.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.hack.azure.mediknot.entity.Address;
+import com.hack.azure.mediknot.enums.MedicalEventType;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Embedded;
 import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class MedicalDto {
 
     private Integer id;
+
+    private PatientDto patientDto;
+
     private LocalDate startDate;
     private LocalDate endDate;
     private Address location;
     private Boolean isActive;
+    private MedicalEventType critical;
 
 }
