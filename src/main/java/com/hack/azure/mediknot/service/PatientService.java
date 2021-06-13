734864562bf6532c0b95719f67bbe6c1cf036269
Malay Saxena <dynamic.malay@gmail.com>
2021-06-13 15:16:23 +0530

    PatientService functions added

--- src/main/java/com/hack/azure/mediknot/service/PatientService.java
+++ src/main/java/com/hack/azure/mediknot/service/PatientService.java
@@ -1,6 +1,9 @@
 package com.hack.azure.mediknot.service;
 
 import com.hack.azure.mediknot.entity.Patient;
+import com.hack.azure.mediknot.entity.Report;
+
+import java.util.List;
 
 public interface PatientService {
 
@@ -9,5 +12,11 @@ public interface PatientService {
     public Patient updatePatientById(Integer id, Patient patient);
 
     public Patient createPatient(Patient patient);
+
+    public Patient addReports(Integer id, List<Report> reports);
+
+    public Patient addAllergies(Integer id, List<String> allergies);
+
+    public void clearReports(Integer id);
 
 }
--- src/main/java/com/hack/azure/mediknot/service/PatientServiceImpl.java
+++ src/main/java/com/hack/azure/mediknot/service/PatientServiceImpl.java
@@ -1,10 +1,16 @@
 package com.hack.azure.mediknot.service;
 
 import com.hack.azure.mediknot.entity.Patient;
+import com.hack.azure.mediknot.entity.Report;
 import com.hack.azure.mediknot.entity.User;
 import com.hack.azure.mediknot.exception.PatientException;
+import com.hack.azure.mediknot.exception.UserException;
 import com.hack.azure.mediknot.repository.PatientRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PatientServiceImpl implements PatientService{
@@ -21,13 +27,54 @@ public class PatientServiceImpl implements PatientService{
 
     @Override
     public Patient updatePatientById(Integer id, Patient patient) {
-        return null;
+        Patient existingPatient;
+        try{
+            existingPatient = getPatientById(id);
+        }catch (UserException e){
+            throw new UserException("Couldn't update, patient not found", 404);
+        }
+        BeanUtils.copyProperties(patient, existingPatient);
+        return patientRepository.save(existingPatient);
     }
 
     @Override
     public Patient createPatient(Patient patient) throws PatientException {
         if(patientRepository.existsByEmailId(patient.getEmailId())){
             throw new PatientException("Patient with email id already exists", 409);
+        }
+        return patientRepository.save(patient);
+    }
+
+    @Override
+    public Patient addReports(Integer id, List<Report> reports) throws PatientException{
+        Patient patient = getPatientById(id);
+        if(patient.getGeneralReports() != null){
+            patient.setGeneralReports(new ArrayList<>());
+        }
+        patient.getGeneralReports().addAll(reports);
+        return updatePatient(patient);
+    }
+
+
+
+    @Override
+    public Patient addAllergies(Integer id, List<String> allergies) {
+        return null;
+    }
+
+    @Override
+    public void clearReports(Integer id) throws UserException{
+        Patient patient = getPatientById(id);
+        if(patient.getGeneralReports()==null){
+            return;
+        }
+        patient.getGeneralReports().clear();
+        updatePatient(patient);
+    }
+
+    private Patient updatePatient(Patient patient) throws UserException {
+        if(patient.getId()==null){
+            throw new UserException("Id not present in Patient body", 204);
         }
         return patientRepository.save(patient);
     }
