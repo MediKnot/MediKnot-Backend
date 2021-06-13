6f661a59c66a52d50c1fba95929febfbb454158b
Ajinkya Taranekar <ajinkyataranekar@gmail.com>
2021-06-13 23:20:27 +0530

    [UPDATE] Test API updated

--- src/main/java/com/hack/azure/mediknot/controller/TestController.java
+++ src/main/java/com/hack/azure/mediknot/controller/TestController.java
@@ -5,11 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+
 @RestController
 @RequestMapping("/test")
 public class TestController {
-    @GetMapping
-    public ResponseEntity<String> testAPI(){
-        return ResponseEntity.ok("Server is up");
+
+    @GetMapping("/check-server")
+    public ResponseEntity<HashMap<String, Object>> checkServer(){
+        HashMap<String, Object> response = new HashMap<>();
+        response.put("Message", "Server is up");
+        response.put("Status", true);
+        return ResponseEntity.ok(response);
     }
 }
