53bd9f01d2a8ded67eb1fb6728f2a5c1b6f7fdc7
Malay Saxena <dynamic.malay@gmail.com>
2021-06-13 15:25:47 +0530

    DoctorService functions added

--- src/main/java/com/hack/azure/mediknot/service/DoctorService.java
+++ src/main/java/com/hack/azure/mediknot/service/DoctorService.java
@@ -5,5 +5,17 @@ import com.hack.azure.mediknot.entity.Doctor;
 public interface DoctorService {
 
     public Doctor createDoctor(Doctor doctor);
+
     public Doctor getDoctorById(Integer id);
+
+    public Doctor updateDoctorById(Integer id, Doctor doctor);
+
+    public Doctor addDegree(Integer id, String degree);
+
+    public Doctor addSpecialization(Integer id, String specialization);
+
+    public Doctor addMedicalCouncilAffiliation(Integer id, String medicalCouncilAffiliation);
+
+    public Doctor addClinicName(Integer id, String clinicName);
+
 }
