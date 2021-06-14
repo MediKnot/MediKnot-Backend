e92172bf3ee305e31a832eb5f2110fcc0b5ad86c
Malay Saxena <dynamic.malay@gmail.com>
2021-06-14 11:32:36 +0530

    PrescriptionService added

--- src/main/java/com/hack/azure/mediknot/service/PrescriptionService.java
+++ src/main/java/com/hack/azure/mediknot/service/PrescriptionService.java
@@ -1,4 +1,20 @@
 package com.hack.azure.mediknot.service;
 
+import com.hack.azure.mediknot.entity.Dosage;
+import com.hack.azure.mediknot.entity.Prescription;
+
+import java.util.List;
+
 public interface PrescriptionService {
+
+    public Prescription addPrescription(Integer consultationId, Prescription prescription);
+
+    public Prescription updatePrescriptionById(Integer id, Prescription prescription);
+
+    public Prescription addDosages(Integer id, List<Dosage> dosageList);
+
+    public Prescription clearDosages(Integer id);
+
+    public void removePrescription(Integer id);
+
 }
