1fbfa826b30bc6c53a338c354c427ebba80f9b53
Malay Saxena <dynamic.malay@gmail.com>
2021-06-15 16:07:24 +0530

    Bean not null copy added

---
+++ src/main/java/com/hack/azure/mediknot/config/BeanNotNullCopy.java
@@ -0,0 +1,33 @@
+package com.hack.azure.mediknot.config;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class BeanNotNullCopy {
+
+    public static void copyNonNullProperties(Object source, Object destination){
+        BeanUtils.copyProperties(source, destination,
+                getNullPropertyNames(source));
+    }
+
+    private static String[] getNullPropertyNames (Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
+        Set emptyNames = new HashSet();
+        for(java.beans.PropertyDescriptor pd : pds) {
+            //check if value of this property is null then add it to the collection
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue == null) emptyNames.add(pd.getName());
+        }
+        String[] result = new String[emptyNames.size()];
+        return (String[]) emptyNames.toArray(result);
+    }
+
+}
+
