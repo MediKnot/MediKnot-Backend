999f8c92b51a4b01da71405c06e96bbafa42e5b2
Malay Saxena <dynamic.malay@gmail.com>
2021-06-22 18:12:39 +0530

    Heart Rate field added

--- src/main/java/com/hack/azure/mediknot/entity/Patient.java
+++ src/main/java/com/hack/azure/mediknot/entity/Patient.java
@@ -6,12 +6,30 @@ import lombok.Setter;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import java.time.LocalDate;
 import java.util.List;
 
 @Entity
 @Getter
 @Setter
 public class Patient extends User{
+
+    private String bloodGroup;
+    private Float weight;
+    private Float height;
+
+    @ElementCollection
+    private List<HealthFields> heartRate;
+
+    @ElementCollection
+    private List<HealthFields> bloodPressure;
+
+    @ElementCollection
+    private List<HealthFields> sugarLevel;
+
+    @ElementCollection
+    private List<HealthFields> haemoglobin;
+
 
     @OneToMany(mappedBy = "patient")
     private List<MedicalEvent> timeline;
