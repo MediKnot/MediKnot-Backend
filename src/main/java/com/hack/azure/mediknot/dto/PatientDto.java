a98ddcf596b9872908ac27a04cf470ceb99068b4
Malay Saxena <dynamic.malay@gmail.com>
2021-06-22 18:34:59 +0530

    Heart Rate added to dto

--- src/main/java/com/hack/azure/mediknot/dto/PatientDto.java
+++ src/main/java/com/hack/azure/mediknot/dto/PatientDto.java
@@ -1,6 +1,7 @@
 package com.hack.azure.mediknot.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.hack.azure.mediknot.entity.HealthFields;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +11,18 @@ import java.util.List;
 @Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class PatientDto extends UserDto {
+
+    private String bloodGroup;
+    private Float weight;
+    private Float height;
+
+    private List<HealthFields> heartRate;
+
+    private List<HealthFields> bloodPressure;
+
+    private List<HealthFields> sugarLevel;
+
+    private List<HealthFields> haemoglobin;
 
     private List<MedicalEventDto> timeline;
     private List<String> allergies;
