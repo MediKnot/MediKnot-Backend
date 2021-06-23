b3fe636545d0becbcb9249ee68509177c982b7c3
Malay Saxena <dynamic.malay@gmail.com>
2021-06-23 19:54:04 +0530

    ProfileViewsDto updated

--- src/main/java/com/hack/azure/mediknot/dto/ProfileViewsDto.java
+++ src/main/java/com/hack/azure/mediknot/dto/ProfileViewsDto.java
@@ -1,7 +1,14 @@
 package com.hack.azure.mediknot.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ProfileViewsDto {
 
     private Integer id;
